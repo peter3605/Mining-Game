4ce56fb99ed4714c3dcea7b4fd1facc43450e650
Peter Ho <peter.ho433@gmail.com>
2019-06-12 12:07:07 -0400

    Testing GIT Bash

--- Block.java
+++ Block.java
@@ -4,11 +4,15 @@ import javax.swing.JComponent;
 
 public class Block extends Rectangle{
 	Block(int a,int b, int c, int d){
-		x=a;
-		y=b;
+		x = a;
+		y = b;
 		width = c;
 		height = d;
 	}
-	public void setX(int i){x=i;}
-	public void setY(int i){y=i;}
+	public void setX(int i){
+		x = i;
+	}
+	public void setY(int i){
+		y = i;
+	}
 }
